36ae7778c941b11419ab9e005ca87b01ae48d4d9
Rupesh <rupesh1@gmail.com>
2019-05-01 11:22:59 +0100

    Use model on hello endpoint

--- src_functions/hello.js
+++ src_functions/hello.js
@@ -1,7 +1,17 @@
+import brain from './neuralNet/trained-net-source';
+
 exports.handler = function(event, context, callback) {
-  console.log("hello CityJS")
+
+  const input = JSON.parse(event.body);
+  const output = brain(input);
+  const ordered_output = [...output].sort((a,b)=> b - a);
+  console.log("input is", input);
+  console.log("output is", output);
+  console.log("ordered_output is", ordered_output);
+
+
   callback(null, {
     statusCode: 200,
-    body: "Hello, CityJS"
+    body: output.indexOf(ordered_output[0]).toString()
   });
 }
